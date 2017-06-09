60a54d7d140635f6277508f78844196eb8575c1e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-06-09 14:44:28 +0300

    Fix test

--- tests/Nerd/Proxy/ProxyTest.php
+++ tests/Nerd/Proxy/ProxyTest.php
@@ -17,6 +17,7 @@ class ProxyTest extends TestCase
                 if ($name == 'barMethod4') {
                     return 10;
                 }
+                return null;
             }
         }, $interfaces, null);
 
